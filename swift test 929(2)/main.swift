db5ebd712e03c060a87e9e1b215f636f084f00a8
KLIZN11 <s20171103188@King-2.local>
2018-09-29 09:37:15 +0800

    swift-test-929(2)
    冒泡排序

--- swift test 929(2)/main.swift
+++ swift test 929(2)/main.swift
@@ -7,6 +7,16 @@
 //
 
 import Foundation
-
-print("Hello, World!")
-
+var array:[Int]=[9,5,2,6,3,7,1,8,0,4]
+for i in 0..<array.count{
+    for j in 0..<array.count-(i+1){
+        if array[j]>array[j+1]{
+            let temp=array[j]
+            array[j]=array[j+1]
+            array[j+1]=temp
+        }
+    }
+}
+for i in 0..<array.count{
+    print(array[i])
+}
